6534752d037986eb8804322bf5d576289fbe6d1d
Letícia de Carvalho <leticia.cs1948@gmail.com>
2025-02-14 12:47:08 -0300

    Associando pagamento a pessoa.java
    Signed-off-by: Letícia de Carvalho <leticia.cs1948@gmail.com>

--- src/entidades/Pagamento.java
+++ src/entidades/Pagamento.java
@@ -1,34 +1,40 @@
 package entidades;
 
 public class Pagamento {
-private double valor;
-private static boolean statusPagamento ;
+    private double valor;
+    private boolean statusPagamento; 
+    Pessoa pessoa;
+    
+    public Pagamento(double valor) {
+        this.valor = valor;
+        this.statusPagamento = false; 
+       
+    }
+    public Paciente getPaciente() {
+        if (this.pessoa instanceof Paciente) { // se a pessoa é um paciente
+            return (Paciente) this.pessoa;    // cast para Paciente
+        }
+        return null;
+    }
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+   
+    public Pagamento() {
+        this.statusPagamento = false; 
+    }
 
+    
+    public double getValor() {
+        return valor;
+    }
 
-Pagamento(double valor){
-	this.valor = valor;
-	Pagamento.statusPagamento = true;
-	
-}
+    public boolean getStatusPagamento() {
+        return statusPagamento;
+    }
 
-public Pagamento() {
-	// TODO Auto-generated constructor stub
-}
-
-public double getValor() {
-    return valor;
-}
-
-public static boolean getStatusPagamento() {
-    return statusPagamento;
-}
-public void realizarPagamento () {
-		statusPagamento= false;
-}
-
-public Pessoa getPaciente() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getPaciente'");
-}
-
+    
+    public void realizarPagamento() {
+        this.statusPagamento = true;
+    }
 }
